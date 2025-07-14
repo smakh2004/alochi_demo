12a160304f04d886529dc4f08349a3579cc1f7fe
Sirojiddin <s.makhammatov@student.inha.uz>
2025-07-14 18:49:51 +0500

    First commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "926342170"
+    serviceWorkerVersion: "3825594761"
   }
 });
